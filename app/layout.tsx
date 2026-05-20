02b8f6f25e1397ba9f677252f1759b4ccf5f1d28
PrzemekAbramowicz <65355720+PrzemekAbramowicz@users.noreply.github.com>
2026-05-20 18:13:17 +0200

    Update dependencies part2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,25 +3,25 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-poppins",
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
-  title: "StorVi",
-  description:
-    "StorVi - a cloud storage app for uploading, organizing and managing files.",
+    title: "StorVi",
+    description:
+        "StorVi - a cloud storage app for uploading, organizing and managing files.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en">
-      <body className={`${poppins.variable} font-poppins antialiased`}>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${poppins.variable} font-poppins antialiased`}>
+                {children}
+            </body>
+        </html>
+    );
 }
--- prettier.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindConfig: "./tailwind.config.ts",
-  tailwindFunctions: ["cn", "cva"],
-};
-
-export default config;
