6719f529b65fa346e82045fa9d74cdb796f8a76c
Przemek Abramowicz <65355720+PrzemekAbramowicz@users.noreply.github.com>
2026-06-07 14:11:32 +0200

    Add actions dropdown

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Thumbnail from "./Thumbnail";
 import { constructFileUrl, convertFileSize } from "@/lib/utils";
 import FormattedDateTime from "./FormattedDateTime";
+import ActionDropdown from "./ActionDropdown";
 
 const Card = ({ file }: { file: Models.Document }) => {
     const fileUrl = constructFileUrl(file.bucketFileId);
@@ -20,7 +21,7 @@ const Card = ({ file }: { file: Models.Document }) => {
                 />
 
                 <div className="flex flex-col items-end justify-between">
-                    ActionsDropdown
+                    <ActionDropdown file={file} />
                     <p className="body-1">{convertFileSize(file.size)}</p>
                 </div>
             </div>
