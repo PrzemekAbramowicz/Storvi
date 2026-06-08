b92d6ef83c71087765bca67cc9ccf2e3ac2d5115
Przemek Abramowicz <65355720+PrzemekAbramowicz@users.noreply.github.com>
2026-06-08 10:13:35 +0200

    Fix: improve file upload UI and config

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -14,13 +14,13 @@ const Toaster = ({ ...props }: ToasterProps) => {
             className="toaster group"
             toastOptions={{
                 classNames: {
-                    toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-                    error: "border-transparent bg-red text-white [&_[data-description]]:text-white [&_[data-icon]]:text-white [&_[data-title]]:text-white",
-                    description: "group-[.toast]:text-muted-foreground",
+                    toast: "group toast group-[.toaster]:border-light-300 group-[.toaster]:bg-white group-[.toaster]:text-dark-200 group-[.toaster]:shadow-lg",
+                    error: "!border-transparent !bg-red !text-white [&_[data-description]]:!text-white [&_[data-icon]]:!text-white [&_[data-title]]:!text-white",
+                    description: "group-[.toast]:text-light-100",
                     actionButton:
-                        "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+                        "group-[.toast]:bg-brand group-[.toast]:text-white",
                     cancelButton:
-                        "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+                        "group-[.toast]:bg-light-300 group-[.toast]:text-light-100",
                 },
             }}
             {...props}
