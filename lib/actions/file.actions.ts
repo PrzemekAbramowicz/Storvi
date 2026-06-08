a3fb62590ea2230abfe7120ad62b3bcfb1116c05
Przemek Abramowicz <65355720+PrzemekAbramowicz@users.noreply.github.com>
2026-06-08 16:09:17 +0200

    Implement global search and filtering based on types

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -57,7 +57,13 @@ export const uploadFile = async ({ file, ownerId, path }: UploadFileProps) => {
     }
 };
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (
+    currentUser: Models.Document,
+    types: string[],
+    searchText: string,
+    sort: string,
+    limit?: number,
+) => {
     const queries = [
         Query.or([
             Query.equal("owner", [currentUser.$id]),
@@ -65,12 +71,25 @@ const createQueries = (currentUser: Models.Document) => {
         ]),
     ];
 
-    // TODO - search, sort, limits
+    if (types.length > 0) queries.push(Query.equal("type", types));
+    if (searchText) queries.push(Query.contains("name", searchText));
+    if (limit) queries.push(Query.limit(limit));
+
+    const [sortBy, orderBy] = sort.split("-");
+
+    queries.push(
+        orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy),
+    );
 
     return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({
+    types = [],
+    searchText = "",
+    sort = "$createdAt-desc",
+    limit,
+}: GetFilesProps) => {
     const { databases } = await createAdminClient();
 
     try {
@@ -78,9 +97,13 @@ export const getFiles = async () => {
 
         if (!currentUser) throw new Error("User not found");
 
-        const queries = createQueries(currentUser);
-
-        console.log({ currentUser, queries });
+        const queries = createQueries(
+            currentUser,
+            types,
+            searchText,
+            sort,
+            limit,
+        );
 
         const files = await databases.listDocuments(
             appwriteConfig.databaseId,
